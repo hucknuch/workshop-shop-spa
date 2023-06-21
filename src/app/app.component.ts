4bd3e0b1f1faf62861f7e8f8e524fcac372288cd
Hassan Khousheish <hassan.khousheish@crunchr.com>
2023-06-22 01:44:21 +0200

    Prepare Workshop Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'wss-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenubarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'workshop-shop';
+  items: MenuItem[] = [
+    {
+      label: 'Shop',
+      icon: 'pi pi-home',
+    },
+    {
+      label: 'Cart',
+      icon: 'pi pi-shopping-cart',
+    },
+  ];
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects(),
+    importProvidersFrom(HttpClientModule),
+  ]
 };
